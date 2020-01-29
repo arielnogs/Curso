2f7fea8ac8e116ac069d71f10e6f2054ed1d4a24
Ariel Nogueira <ariel.arinog@gmail.com>
2020-01-29 18:27:49 -0300

    Exercicio finalizado NotaAluno

---
+++ ExercicioNotaAluno/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioNotaAluno
+{
+    class Aluno
+    {
+        public double Nota1, Nota2, Nota3, NotaFinal, Restam;
+        public string resultfim, Nome;
+
+        public void Resultado()
+        {
+            NotaFinal = (Nota1 + Nota2 + Nota3);
+
+            if (NotaFinal >= 60.00)
+            {
+                resultfim = "APROVADO";
+            }
+            else
+            {
+
+                Restam = 60.00 - NotaFinal;
+                resultfim = "REPROVADO FALTAM " + Restam.ToString("F2") + " PONTOS";
+
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "Nota Final=" + NotaFinal.ToString("F2", CultureInfo.InvariantCulture) + " " +
+                resultfim;
+        }
+    }
+}
---
+++ ExercicioNotaAluno/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioNotaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno A = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            A.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            A.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Resultado();
+            Console.Write(A);
+
+
+
+
+        }
+    }
+}
